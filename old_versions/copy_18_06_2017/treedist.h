ef009bc0def6502ebad7d486a90fe6b748e5fd97
Mykola_Lebid <mykola.lebid@gmail.com>
2017-06-18 15:55:44 +0200

    Phylip_dist

---
+++ old_versions/copy_18_06_2017/treedist.h
@@ -0,0 +1,10 @@
+#ifndef TREE_DIST_H_INCLUDED
+#define TREE_DIST_H_INCLUDED
+
+#include "cons.h"
+#include <string>
+
+float phylip_dist(std::string & Probe_mut_1,
+								  std::string & Probe_mut_2, int dist_type);
+
+#endif // TREE_DIST_H
