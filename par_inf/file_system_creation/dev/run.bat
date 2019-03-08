f63678e9d1f0a2da34bc28acdd966e500f41cac2
Mykola Lebid <mykola.lebid@gmail.com>
2019-03-08 15:43:29 +0200

    Rename par_inf/file_system_creation/dev/run.bat to par_inf/file_system_creation/source/run.bat

--- par_inf/file_system_creation/dev/run.bat
+++
@@ -1,2 +0,0 @@
-chmod +x file_system_creation.exe
- ./file_system_creation.exe $1 $2 $3 $4 $5 $6 $7 $8
---
+++ par_inf/file_system_creation/source/run.bat
@@ -0,0 +1,2 @@
+chmod +x file_system_creation.exe
+ ./file_system_creation.exe $1 $2 $3 $4 $5 $6 $7 $8
